9174a2f9efd453d6e0ffbcd47e9698cc2db481f6
MCamacho4207 <mcamacho4207@gmail.com>
2026-03-09 18:58:38 +0000

    Fixed typo in frontend proxy setup

--- frontend-ui/src/setupProxy.js
+++ frontend-ui/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/url-shortener',
     createProxyMiddleware({
-      target: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';,
+      target: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080',
       changeOrigin: true,
       pathRewrite: (path, req) => {
               return path.startsWith('/url-shortener') ? path : `/url-shortener${path}`;
