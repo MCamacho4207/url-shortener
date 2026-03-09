12479090ed921c2f8b6ef3d322c9c050a0ce8c6c
MCamacho4207 <mcamacho4207@gmail.com>
2026-03-09 16:07:12 +0000

    Docker config added for running the applications as a Docker container with network config

--- frontend-ui/src/setupProxy.js
+++ frontend-ui/src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+
   app.use(
     '/url-shortener',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';,
       changeOrigin: true,
       pathRewrite: (path, req) => {
               return path.startsWith('/url-shortener') ? path : `/url-shortener${path}`;
